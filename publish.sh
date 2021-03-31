d9ab6391f8c8304217d8b91a6fd767dd8d0cb477
Evan Pratten <ewpratten@gmail.com>
2021-03-31 08:36:20 -0400

    update metadata

--- publish.sh
+++ publish.sh
@@ -3,12 +3,12 @@
 set -e
 
 echo "Publishing core library"
-cargo publish --manifest-path libpolymesh/Cargo.toml
+cargo publish --manifest-path libpolymesh/Cargo.toml $@
 
 echo "Publishing all tools"
-cargo publish --manifest-path tools/pmfcube/Cargo.toml
-cargo publish --manifest-path tools/pmfextract/Cargo.toml
-cargo publish --manifest-path tools/pmfpack/Cargo.toml
-cargo publish --manifest-path tools/pmftree/Cargo.toml
-cargo publish --manifest-path tools/pmfview/Cargo.toml
-cargo publish --manifest-path tools/vox2pmf/Cargo.toml
+cargo publish --manifest-path tools/pmfcube/Cargo.toml $@
+cargo publish --manifest-path tools/pmfextract/Cargo.toml $@
+cargo publish --manifest-path tools/pmfpack/Cargo.toml $@
+cargo publish --manifest-path tools/pmftree/Cargo.toml $@
+cargo publish --manifest-path tools/pmfview/Cargo.toml $@
+cargo publish --manifest-path tools/vox2pmf/Cargo.toml $@
